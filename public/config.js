04e8e4439cf47d8feb755ba95861f1c4fcd5a50c
Mario Reder <mario.reder@pm.me>
2026-05-23 04:36:09 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "Decentralized trading, Launchpad, lightning-fast swaps on Solana.",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Trading,Markets,Vaults,Rewards,Points,Leaderboard,Portfolio,Campaigns",
+  "VITE_ENABLED_MENUS": "Trading,Markets,Vaults,Rewards,Points,Leaderboard,Portfolio,Campaigns,Swap",
   "VITE_CUSTOM_MENUS": "MoonLab, https://solanicafinance.app; Docs, https://solanica-finance.gitbook.io/solanica-finance-docs/",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
