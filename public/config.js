8577e764198c9b23c71c3d68ccae72d8876cc50f
Mario Reder <mario.reder@pm.me>
2026-05-29 05:51:12 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Markets,Vaults,Rewards,Points,Leaderboard,Portfolio,Campaigns,Swap",
-  "VITE_CUSTOM_MENUS": "MoonLab, https://solanicafinance.app; Docs, https://solanica-finance.gitbook.io/solanica-finance-docs/",
+  "VITE_CUSTOM_MENUS": "MoonLab, https://solanicafinance.app; Docs, https://solanica-finance.gitbook.io/solanica-finance-docs/; Buy $SFTN, https://dexscreener.com/solana/AudrBfor6anzDFtgbbNvDHCathxQVh1Qqu3RvehUbonk",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/SolanicaFinance",
